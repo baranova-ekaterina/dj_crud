fd39e091b3179d323054a7c7bf1b43fd4ff418e0
Baranova Ekaterina <recrane@rambler.ru>
2023-06-26 17:32:50 +1000

    change file

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': ''
+        'PASSWORD': 'Darinka1308'
     }
 }
 
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
